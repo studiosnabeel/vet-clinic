47a2d0ed3e55ca81990ab1ffd8af4675189e3549
Nabeel Akbar <studiosnabeel@gmail.com>
2023-01-26 13:06:07 +0500

    Inserts into owners and species

--- data.sql
+++ data.sql
@@ -22,3 +22,18 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', 'May 14, 2022', 4, true, 22);
 
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+
+INSERT INTO species (name) VALUES ('Digimon');
