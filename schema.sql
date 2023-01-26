0668b4204f558e1cb9bb26d654ffd69b30c07d01
Nabeel Akbar <studiosnabeel@gmail.com>
2023-01-26 13:00:59 +0500

    Alter animals table

--- schema.sql
+++ schema.sql
@@ -26,3 +26,23 @@ CREATE TABLE species (
     name VARCHAR(100),
     PRIMARY KEY (id)
 );
+
+ALTER TABLE animals
+    ADD PRIMARY KEY(id);
+
+ALTER TABLE animals
+    DROP COLUMN species;
+
+ALTER TABLE animals
+    ADD COLUMN species_id INT,
+    ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+    ADD CONSTRAINT fkey_species
+    FOREIGN KEY (species_id)
+    REFERENCES species(id);
+
+ALTER TABLE animals
+    ADD CONSTRAINT fkey_owner
+    FOREIGN KEY (owner_id)
+    REFERENCES owners(id);
