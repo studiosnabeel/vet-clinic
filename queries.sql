30bfe48af09c6628f9b2a02c44ef20fe509f6be2
187jjay187 <187jjay187@gmail.com>
2023-02-01 16:35:51 +0200

    Add modified queries.sql file

--- queries.sql
+++ queries.sql
@@ -172,3 +172,4 @@ LIMIT 1;
 
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 15;
 EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
